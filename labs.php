ff091ac990f45aa9c9c7e9e04a6e44a427a6c688
Thomas Lenz <thomas.lenz96@gmail.come>
2017-10-02 13:37:46 -0500

    Changed all php files to source files from my core GitHub repo, allowing could syncing

--- labs.php
+++ labs.php
@@ -3,37 +3,10 @@
 	@date 28 Sept 2017
 	@project Azure Website
 	@file labs.php
- 	@course Web and Distributed Programming
-	@semester Fall 2017
+	@course Web and Dist Programming
+	@semester Fall, 2017
 -->
 
-<!DOCTYPE=html>
-<html>
-	<head>
-		<!-- Source CSS -->
-		<link rel="stylesheet" type="text/css" href="./azure-website.css"></link>
-		<meta charset="utf-8" />
-		<!-- <title>Web and Distributed Programming Assignment Archive</title> -->
-		<title>WaDPAA</title>
-	</head>
-
-	<!-- Stops the margins from breaking the top navbar -->
-	<body class="bodyStyle">
-
-		<?php
-			include('header.html');
-       		?>
-
-		<h1 style="font-family:sans-serif">Labs</h1>
-		<hr />
-	
-		<p class="defaultText">
-			In-class labs will be listed on this page. Clicking the name of the lab will take you to a webpage displaying the lab. All links below will open in a seperate page.<br /><br />
-			<a href="./deployments/labs/l-01-hello-world/hello-world-tll.php" target="_blank">Lab 1: Hello World</a> <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/tree/master/labs/l-01-hello-world">[GitHub]</a> <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/raw/master/labs/l-01-hello-world/l-01-hello-world.zip">[ZIP]</a><br />
-			<a href="./deployments/labs/l-02-course-schedule/course-schedule.php" target="_blank">Lab 2: Course Schedule</a> <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/tree/master/labs/l-02-course-schedule">[GitHub]</a> <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/raw/master/labs/l-02-course-schedule/l-02-course-schedule.zip">[ZIP]</a><br />
-			<a href="./deployments/labs/l-03-course-schedule-css/course-schedule-css.php" target="_blank">Lab 3: Course Schedule 2 &mdash; Electric Boogaloo</a> <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/tree/master/labs/l-03-course-schedule-css">[GitHub]</a> <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/raw/master/labs/l-03-course-schedule-css/l-03-course-schedule-css.zip">[ZIP]</a><br />
-			<a href="./deployments/labs/l-04-javascript-basics/javascript-basics.php" target="_blank">Lab 4: JavaScript Basics</a> <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/tree/master/labs/l-04-javascript-basics">[GitHub]</a><br />
-		</p>
-	</body>
-
-</html>
+<?php
+include("https://raw.githubusercontent.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/master/projects/p-02-first-website/labs.php");
+?>
