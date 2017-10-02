fb577ac8746c2a346ec37670bf3cbcdc584afd20
Thomas Lenz <thomas.lenz96@gmail.come>
2017-10-02 14:15:30 -0500

    Remote-sourced banner file

---
+++ header.php
@@ -0,0 +1,12 @@
+<!--
+	@author Thomas Lenz <thomas.lenz96@gmail.com> AS The2b
+	@date 28 Sept 2017
+	@project Azure Website
+	@file header.php
+ 	@course Web and Distributed Programming
+	@semester Fall 2017
+-->
+
+<?php
+include("https://raw.githubusercontent.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/master/projects/p-02-first-website/header.html");
+?>
--- projects.php
+++ projects.php
@@ -8,5 +8,5 @@
 -->
 
 <?php
-include("https://raw.githubusercontent.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/master/projects/p-02-first-website/labs.php");
+include("https://raw.githubusercontent.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/master/projects/p-02-first-website/projects.php");
 ?>
