9c5ac83b0ec6727b7a150337de3a54d4e341658c
Thomas Lenz <thomas.lenz96@gmail.come>
2017-10-12 03:57:32 -0500

    Added in Lab 5 deployment

---
+++ deployments/labs/l-05-javascript-html-interaction/html-js-html.php
@@ -0,0 +1,19 @@
+<!--
+	@author Thomas Lenz <thomas.lenz96@gmail.com> AS The2b
+	@date 12 October 2017
+	@project HTML & JavaScript Interaction
+	@file html-js.php
+	@course Web and Dist Programming
+	@semester Fall, 2017
+-->
+
+<?php
+$rawHtml=file_get_contents("https://raw.githubusercontent.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/master/labs/l-05-javascript-html-interaction/html-js.html");
+
+$oldScriptLine="<script type=\"text/javascript\" src=\"html-js.js\"></script>";
+$newScriptLine="<script type=\"text/javascript\" src=\"html-js-js.php\"></script>";
+
+$changes=str_replace($oldScriptLine,$newScriptLine,$rawHtml);
+
+echo ($changes);
+?>
---
+++ deployments/labs/l-05-javascript-html-interaction/html-js-js.php
@@ -0,0 +1,7 @@
+<!--
+
+-->
+
+<?php
+	include("https://raw.githubusercontent.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/master/labs/l-05-javascript-html-interaction/html-js.js");
+?>
