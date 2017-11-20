b7c2b85210d25aa1cc93c6235f6ed6d689986595
Thomas Lenz <thomas.lenz96@gmail.com>
2017-11-20 14:58:10 -0600

    Linked Project 4

---
+++ deployments/projects/p-04-contact-manager/contact-manager-html.php
@@ -0,0 +1,11 @@
+<?php
+$css = file_get_contents("https://raw.githubusercontent.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/master/projects/p-04-contact-manager/contact-manager.css");
+$html = file_get_contents("https://raw.githubusercontent.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/master/projects/p-04-contact-manager/contact-manager.html");
+
+$fullStyle = "<style> " . $css . " </style>";
+$styledHtml = str_replace("<link rel=\"stylesheet\" type=\"text/css\" href=\"./contact-manager.css\"></link>",$fullStyle,$html);
+
+$fullHtml = str_replace("contact-manager.js","contact-manager-js.php",$styledHtml);
+
+echo $fullHtml
+?>
---
+++ deployments/projects/p-04-contact-manager/contact-manager-js.php
@@ -0,0 +1,3 @@
+<?php
+include("https://raw.githubusercontent.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/master/projects/p-04-contact-manager/contact-manager.js");
+?>
