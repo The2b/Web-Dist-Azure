81f55691907fdd090e2f2be284afa2c8f27d0f74
Thomas Lenz <thomas.lenz96@gmail.com>
2017-11-21 01:58:39 -0600

    Added help image for Project 4

--- deployments/projects/p-04-contact-manager/contact-manager-html.php
+++ deployments/projects/p-04-contact-manager/contact-manager-html.php
@@ -1,9 +1,9 @@
 <?php
 $css = file_get_contents("https://raw.githubusercontent.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/master/projects/p-04-contact-manager/contact-manager.css");
-$html = file_get_contents("https://raw.githubusercontent.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/master/projects/p-04-contact-manager/contact-manager.html");
+$html = file_get_contents("https://raw.githubusercontent.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/master/projects/p-04-contact-manager/contact-manager.php");
 
 $fullStyle = "<style> " . $css . " </style>";
-$styledHtml = str_replace("<link rel=\"stylesheet\" type=\"text/css\" href=\"./contact-manager.css\"></link>",$fullStyle,$html);
+$styledHtml = str_replace("<link rel=\"stylesheet\" type=\"text/css\" href=\"./contact-manager.css\" />",$fullStyle,$html);
 
 $fullHtml = str_replace("contact-manager.js","contact-manager-js.php",$styledHtml);
 
